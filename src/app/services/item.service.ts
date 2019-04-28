e2ddd23536fc3abbef181cbd9f243204120d07a7
Matias Moreno <matias.moreno@esolutions.com.ar>
2019-04-28 12:03:31 -0300

    Se manejan errores

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -6,6 +6,7 @@ import {HttpHeaders, HttpClient} from '@angular/common/http';
 import {ListItemsMeliResponseDto} from '../dtos/list-items-meli-response/list-items-meli-response.dto';
 import {environment} from '../../environments/environment';
 import {SearchResult} from '../models/search-result';
+import {ServiceError} from './service-error';
 
 @Injectable({
   providedIn: 'root'
@@ -22,9 +23,28 @@ export class ItemService {
     const url = `${this.baseUrl}/items?q=${query}`;
     const httpOptions = {headers: new HttpHeaders().set('Accept', 'application/json')};
 
-    return this.http.get<ListItemsMeliResponseDto>(url, httpOptions)
+    return this.http.get<any>(url, httpOptions)
       .pipe(map(response => {
-        return ListItemsMeliResponseDto.convertToDomain(response);
-      }));
+          if (response.statusCode === 204) {
+            console.log(response);
+          } else {
+            return ListItemsMeliResponseDto.convertToDomain(response);
+          }
+        }),
+        catchError(
+          err => {
+            switch (err.key) {
+              case 400: {
+                return throwError(new ServiceError('examen-tecnico-api-401', 'Ocurrio un error al intentar consultar la api.'));
+              }
+              case 500: {
+                return throwError(new ServiceError('examen-tecnico-api-500', 'Ocurrió un error en el servidor'));
+              }
+              default: {
+                return throwError(new ServiceError('examen-tecnico-api-generico', 'Ocurrió un error'));
+              }
+            }
+          }
+        ));
   }
 }
---
+++ src/app/services/service-error.ts
@@ -0,0 +1,3 @@
+export class ServiceError {
+    constructor( private key: string , public message: string ) { }
+}
